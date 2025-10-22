9e5fd7f58f1c365da9d7536786bd3647f3fe5610
Moiz Bohra <mhb@gmail.com>
2025-10-22 15:50:44 +0530

    Updating DesingPattern

---
+++ src/main/java/com/example/britishtime/formatter/GermanTimeFormatter.java
@@ -0,0 +1,8 @@
+package com.example.britishtime.formatter;
+
+public class GermanTimeFormatter implements TimeFormatterStrategy {
+    @Override
+    public String format(int hour24, int minute) {
+        return "";
+    }
+}
